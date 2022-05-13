81c0b8b2b924d73abadf8942782fa33b2018d316
Taeke d <95484190+Taeke06@users.noreply.github.com>
2022-05-13 13:23:59 +0000

    kies een letter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
-print("Welkom bij Galgje! Galge is een spel waar je een woord moet raden die de computer hier heeft uitgekozen.")
+print("Welkom bij Galgje!\nGalgje is een spel waar je een woord moet raden die de computer hier heeft uitgekozen.")
 woordenlijst= ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit" , "heesterperk"]
 woord= random.choice (woordenlijst)
 print (woord) 
 
-
+letter = input ("kies een letter\n")
