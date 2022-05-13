38b857b7d5ca26eb86fa4cf76b6479340ab2a4e3
Taeke d <95484190+Taeke06@users.noreply.github.com>
2022-05-13 13:21:06 +0000

    random woord

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import random
 print("Welkom bij Galgje! Galge is een spel waar je een woord moet raden die de computer hier heeft uitgekozen.")
+woordenlijst= ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit" , "heesterperk"]
+woord= random.choice (woordenlijst)
+print (woord) 
+
+
